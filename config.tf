e7c374359fe47c71ba23770e2caffd791357588f
EmilyBurak <emily@emilytburak.com>
2023-10-08 16:08:59 -0600

    Setup sample backend

---
+++ config.tf
@@ -0,0 +1,56 @@
+resource "aws_s3_bucket" "terraform_state" {
+  # placeholder development bucket name
+  bucket = "aws-month-cost-datadog-lambda-state"
+
+  # Prevent accidental deletion of S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "tf_state_locks" {
+  # placeholder development table name
+  name         = "aws-month-cost-datadog-lambda-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "aws-month-cost-datadog-lambda-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "aws-month-cost-datadog-lambda-locks"
+    encrypt        = true
+  }
+}
